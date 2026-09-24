b745314e9bb3fb58a0f6cb10984418a1ed2eb333
ニキータ <50665831+nikitanightness@users.noreply.github.com>
2026-09-24 23:35:04 +0300

    m1_v5_us: Change HS_SLEEP_TIMEOUT to 15 seconds (for test)

--- keyboards/monsgeek/m1_v5/m1_v5_us/wls/wls.h
+++ keyboards/monsgeek/m1_v5/m1_v5_us/wls/wls.h
@@ -14,7 +14,7 @@
 #define HS_GET_MODE_PIN(state)            HS_GET_MODE_PIN_(state)
 #define HS_MODEIO_DETECTION_TIME          50
 #define HS_LBACK_TIMEOUT                  (30 * 1000)
-#define HS_SLEEP_TIMEOUT                  (1 * 60000) //(1 * 60000)
+#define HS_SLEEP_TIMEOUT                  15000 // 15 seconds
 
 enum modeio_mode {
     hs_none = 0,
